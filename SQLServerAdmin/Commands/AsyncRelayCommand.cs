92c60beee3782c85c371fca30067d6366b90208a
WeisssiekerAIDev <Weisssieker@hotmail.com>
2025-02-18 06:21:26 +0100

    Add files via upload

---
+++ SQLServerAdmin/Commands/AsyncRelayCommand.cs
@@ -0,0 +1,43 @@
+using System.Windows.Input;
+
+namespace SQLServerAdmin.Commands
+{
+    public class AsyncRelayCommand : ICommand
+    {
+        private readonly Func<Task> _execute;
+        private bool _isExecuting;
+
+        public AsyncRelayCommand(Func<Task> execute)
+        {
+            _execute = execute;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return !_isExecuting;
+        }
+
+        public async void Execute(object? parameter)
+        {
+            if (_isExecuting) return;
+
+            try
+            {
+                _isExecuting = true;
+                CommandManager.InvalidateRequerySuggested();
+                await _execute();
+            }
+            finally
+            {
+                _isExecuting = false;
+                CommandManager.InvalidateRequerySuggested();
+            }
+        }
+    }
+}
